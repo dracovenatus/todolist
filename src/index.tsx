d714832c420e91c9695e45c35a6971c022944f48
valeria-ab <offtotheraces@inbox.ru>
2025-02-13 19:03:00 +0300

    Made imports from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import { Provider } from "react-redux"
-import { App } from "./app/App"
-import { store } from "./app/store"
-import { BrowserRouter, RouterProvider } from "react-router"
-import { router } from "common/routing"
+import { router } from "common/routing";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router";
+import { store } from "./app/store";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-  // <BrowserRouter>
   <Provider store={store}>
     <RouterProvider router={router} />
-    {/* <App /> */}
-  </Provider>,
-  // </BrowserRouter>,
-)
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
